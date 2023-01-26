301f2047e14cf71025574fce1c3fdfc9db8d5a45
Edu Gonzalez Garcia <71343560+edgonga@users.noreply.github.com>
2023-01-26 14:02:19 +0100

    Update index.js

--- (M4) Exercici EXTRA 3. Juguem a pedra, paper o tisora/index.js
+++ (M4) Exercici EXTRA 3. Juguem a pedra, paper o tisora/index.js
@@ -1,18 +1,61 @@
-let randomNum = Math.floor(Math.random() * 3) + 1
-let userNum = Number(document.getElementById("selection-dropdown").value)
-let randomObject = document.getElementsByName(randomNum.toString())
-let userObject = document.getElementsById("selection-dropdown").name
-let result
+const play = () => {
 
-const play = (selectionNum, notSelectedNum) => {
+    let randomNum = Math.floor(Math.random() * 3) + 1
+    let userNum = parseInt(document.getElementById("selection-dropdown").value)
+    //let randomObject = document.getElementsByName(randomNum.toString())
+    //let userObject = document.getElementsById("selection-dropdown").name
+    let result
 
-    selectionNum = userNum
-    notSelectedNum = randomNum
+    if (userNum === 1) {
+        if (randomNum === 1) {
+            result = "Jo he tret pedra i tu pedra. Empat!"
+            
+        }
+        if (randomNum === 2) {
+            result = "Jo he tret pedra i tu paper. Has guanyat..!"
+            
+        }
+        if (randomNum === 3) {
+            result = "Jo he tret pedra i tu tisores. He guanyat :)"
+            
+        }
+    }
 
-    switch (true) {
-        case (selectionNum === 1 && notSelectedNum === 1):
-            result = 
+    if (userNum === 2) {
+        if (randomNum === 1) {
+            result = "Jo he tret paper i tu pedra. He guanyat"
+            
+        }
+        if (randomNum === 2) {
+            result = "Jo he tret paper i tu paper. Empat!"
+            
+        }
+        if (randomNum === 3) {
+            result = "Jo he tret paper i tu tisores. Has guanyat..."
+            
+        }
 
     }
+
+    if (userNum === 3) {
+
+        if (randomNum === 1) {
+            result = "Jo he tret tisores i tu pedra. Has guanyat..."
+            return
+        }
+        if (randomNum === 2) {
+            result = "Jo he tret tisores i tu paper. He guanyat :)"
+            return
+        }
+        if (randomNum === 3) {
+            result = "Jo he tret tisores i tu tisores. Empat!"
+            return
+        }
+    }
+        
+    
+
+    
+    document.getElementById("result-message").innerHTML = result
 
 }
