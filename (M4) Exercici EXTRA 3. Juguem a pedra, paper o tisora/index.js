2ae72385aea641a616c03e86417495d08fbce90e
edgonga <gonzalez.edu.duc@gmail.com>
2023-01-26 13:04:08 +0100

    fonaments 26012023 (2)

---
+++ (M4) Exercici EXTRA 3. Juguem a pedra, paper o tisora/index.js
@@ -0,0 +1,18 @@
+let randomNum = Math.floor(Math.random() * 3) + 1
+let userNum = Number(document.getElementById("selection-dropdown").value)
+let randomObject = document.getElementsByName(randomNum.toString())
+let userObject = document.getElementsById("selection-dropdown").name
+let result
+
+const play = (selectionNum, notSelectedNum) => {
+
+    selectionNum = userNum
+    notSelectedNum = randomNum
+
+    switch (true) {
+        case (selectionNum === 1 && notSelectedNum === 1):
+            result = 
+
+    }
+
+}
