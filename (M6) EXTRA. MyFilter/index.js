5f64d55f2277fc397ab267894dd9b5b9df4d966e
edgonga <gonzalez.edu.duc@gmail.com>
2023-02-16 14:00:24 +0100

    movies 1st try

--- (M6) EXTRA. MyFilter/index.js
+++ (M6) EXTRA. MyFilter/index.js
@@ -8,9 +8,11 @@ function myFilter (myArray, conditionFunc) {
     if (!typeof(conditionFunc) === "function") {
         return "El segundo parámetro debe ser una función"
     }
-
+    
     for (let index = 0; index < myArray.length; index++) {
-        newArray.push(conditionFunc(myArray[index]))
+        if (conditionFunc(myArray[index])) {
+            newArray.push(conditionFunc(myArray[index]))
+        } 
     }
     return newArray
 }
@@ -18,14 +20,55 @@ function myFilter (myArray, conditionFunc) {
 function isBiggerThanFive (elementOfMyArray) {
     if (elementOfMyArray > 5) {
         return elementOfMyArray
+    }    
+}
+
+function isEvenNumber (numOfMyArray) {
+    if (numOfMyArray % 2 === 0) {
+        return numOfMyArray
     }
-    else {
-        //pop?
+}
+
+function startsByB (city) {
+    if (city[0] === "B") {
+        return city
     }
-    
+}
+
+function isGrilloFamily (key) {
+    if (key.surname === "Grillo") {
+        const values = {name: key.name, surname: key.surname}
+        return values
+    }
 }
 
 const nums = [1, 4, 9, 2, 22]
-const result = myFilter(nums, isBiggerThanFive)
+const grilloFamily = [
+    { name: 'Pepita', surname: 'Grillo', age: 95 },
+    { name: 'Maria', surname: 'Martinez', age: 35 },
+    { name: 'Pepito', surname: 'Grillo', age: 110 },
+    { name: 'Pepito', surname: 'Sanchez', age: 45 }
+ ]
 
-console.log(result);
+const arrayOfCities = ['Barcelona','Paris','Roma','Londres','Berlin']
+const evenOrOdd = [1,2,3,4,5,6,7,8,9,10]
+
+const resultBiggerThanFive = myFilter(nums, isBiggerThanFive)
+console.log(resultBiggerThanFive);
+
+const resultIsEvenNumber = myFilter(evenOrOdd, isEvenNumber)
+console.log(resultIsEvenNumber);
+
+const resultStartsByB = myFilter(arrayOfCities, startsByB)
+console.log(resultStartsByB);
+
+const resultIsGrilloFamily = myFilter(grilloFamily, isGrilloFamily)
+//console.log(resultIsGrilloFamily);
+
+const grilloFamilyFormated = resultIsGrilloFamily.map(function(object) {
+    return `Name: ${object.name} Surname: ${object.surname}`
+})
+
+
+
+document.getElementById("result-message").innerHTML = `${resultBiggerThanFive} <br> ${resultIsEvenNumber} <br> ${resultStartsByB} <br> ${grilloFamilyFormated}`
